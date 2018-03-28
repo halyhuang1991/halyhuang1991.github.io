c6ffbae446d8e6f66152387e3da4e57ae7286cd8
halyhuang1991 <32833856+halyhuang1991@users.noreply.github.com>
2018-03-28 14:46:29 +0800

    Add files via upload

---
+++ script/jquery-loadmask/jquery.loadmask.js
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2009 Sergiy Kovalchuk (serg472@gmail.com)
+ * 
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *  
+ * Following code is based on Element.mask() implementation from ExtJS framework (http://extjs.com/)
+ *
+ */
+
+;(function($){
+	
+	/**
+	 * Displays loading mask over selected element(s). Accepts both single and multiple selectors.
+	 *
+	 * @param label Text message that will be displayed on top of the mask besides a spinner (optional). 
+	 * 				If not provided only mask will be displayed without a label or a spinner.  	
+	 * @param delay Delay in milliseconds before element is masked (optional). If unmask() is called 
+	 *              before the delay times out, no mask is displayed. This can be used to prevent unnecessary 
+	 *              mask display for quick processes.   	
+	 */
+	$.fn.mask = function(label, delay){
+        delay=1000;//*
+		$(this).each(function() {
+			if(delay !== undefined && delay > 0) {
+		        var element = $(this);
+		        element.data("_mask_timeout", setTimeout(function() { $.maskElement(element, label)}, delay));
+			} else {
+				$.maskElement($(this), label);
+			}
+		});
+	};
+	
+	/**
+	 * Removes mask from the element(s). Accepts both single and multiple selectors.
+	 */
+	$.fn.unmask = function(){
+		$(this).each(function() {
+			$.unmaskElement($(this));
+		});
+	};
+	
+	/**
+	 * Checks if a single element is masked. Returns false if mask is delayed or not displayed. 
+	 */
+	$.fn.isMasked = function(){
+		return this.hasClass("masked");
+	};
+
+	$.maskElement = function(element, label){
+	
+		//if this element has delayed mask scheduled then remove it and display the new one
+		if (element.data("_mask_timeout") !== undefined) {
+			clearTimeout(element.data("_mask_timeout"));
+			element.removeData("_mask_timeout");
+		}
+
+		if(element.isMasked()) {
+			$.unmaskElement(element);
+		}
+
+		
+		if(element.css("position") == "static") {
+			element.addClass("masked-relative");
+		}
+		
+		element.addClass("masked");
+		
+        var maskWrap=$("<div class='maskWrap'></div>");
+		var maskDiv = $('<div class="loadmask"></div>');
+
+		
+		//fix for z-index bug with selects in IE6
+		if(navigator.userAgent.toLowerCase().indexOf("msie 6") > -1){
+			element.find("select").addClass("masked-hidden");
+		}
+		
+        maskWrap.append(maskDiv);
+        element.after(maskWrap); 
+		
+		if(label != undefined) {
+			var maskMsgDiv = $('<div class="loadmask-msg" style="display:none;"></div>');
+			maskMsgDiv.append('<div>' + label + '</div>');
+            maskDiv.after(maskMsgDiv);
+			
+            //alert(element.position().top+"/"+element.position().left);
+            maskWrap.height(element.innerHeight());
+            maskWrap.width(element.innerWidth());
+            maskWrap.css("top",element.position().top+"px");
+            maskWrap.css("left",element.position().left+"px");
+
+            if(element.innerHeight()<maskMsgDiv.innerHeight()) {
+                maskMsgDiv.height(element.innerHeight());
+            }
+
+			//calculate center position
+			maskMsgDiv.css("top", Math.round(element.height() / 2 - (maskMsgDiv.height() - parseInt(maskMsgDiv.css("padding-top")) - parseInt(maskMsgDiv.css("padding-bottom"))) / 2)+"px");
+			maskMsgDiv.css("left", Math.round(element.width() / 2 - (maskMsgDiv.width() - parseInt(maskMsgDiv.css("padding-left")) - parseInt(maskMsgDiv.css("padding-right"))) / 2)+"px");
+			
+			maskMsgDiv.show();
+		}
+		
+	};
+	
+	$.unmaskElement = function(element){
+		//if this element has delayed mask scheduled then remove it
+		if (element.data("_mask_timeout") !== undefined) {
+			clearTimeout(element.data("_mask_timeout"));
+			element.removeData("_mask_timeout");
+		}
+		
+        element.parent().find(".maskWrap").remove();
+		element.removeClass("masked");
+		element.removeClass("masked-relative");
+		element.parent().find("select").removeClass("masked-hidden");
+	};
+ 
+})(jQuery);
