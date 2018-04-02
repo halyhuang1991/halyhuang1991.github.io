d01fd8bb28f8dee09a10c0af29dc3332ad67abc3
halyhuang1991 <18807898837@139.com>
2018-04-02 17:06:48 +0800

    update

--- data/batch/gettxtjson.py
+++ data/batch/gettxtjson.py
@@ -1,12 +1,18 @@
 import os
 import json
+def setlist(str):
+    ls=str.split("\t")
+    ls=list(set(ls))
+    return " ".join(ls)
+
+
 txtfolder = os.path.abspath(os.path.join(os.getcwd(), ".."))+"\\txt"
 arr = []
 for (root, dirs, files) in os.walk(txtfolder):
     for filename in files:
         path = os.path.join(root, filename)
         f = open(path, "r")
-        data = {"name": filename, "text": f.read().replace("\n","<br/>"),"path":"/data/txt/"+filename}
+        data = {"name": filename, "text": setlist(f.read()).replace("\n","<br/>"),"path":"/data/txt/"+filename}
         arr.append(data)
 
 with open('../json/data.json', 'w') as f:
